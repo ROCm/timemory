7a9606c96cdc055cf008b9c10791e6032c393c17
Anuj Shukla <anujshuk@amd.com>
2025-12-04 07:19:57 -0500

    Fix: Replace hard-coded thread limit with TIMEMORY_MAX_STORAGE_THREADS

--- source/timemory/operations/types.hpp
+++ source/timemory/operations/types.hpp
@@ -813,7 +813,7 @@ template <typename T>
 struct set_storage
 {
     friend struct get_storage<T>;
-    static constexpr size_t max_threads = 4096;
+    static constexpr size_t max_threads = TIMEMORY_MAX_STORAGE_THREADS;
     using type                          = T;
     using storage_array_t               = std::array<storage<type>*, max_threads>;
 
