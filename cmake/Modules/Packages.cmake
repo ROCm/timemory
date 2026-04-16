7124c863997eed2efcbaa756e20450c5387048f8
Mikhail <lloginov@amd.com>
2026-04-16 07:27:45 -0400

    update libunwind

--- cmake/Modules/Packages.cmake
+++ cmake/Modules/Packages.cmake
@@ -1138,8 +1138,8 @@ elseif(TIMEMORY_USE_LIBUNWIND AND TIMEMORY_BUILD_LIBUNWIND)
         RECURSIVE
         RELATIVE_PATH external/libunwind
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        REPO_URL https://github.com/jrmadsen/libunwind.git
-        REPO_BRANCH master)
+        REPO_URL https://github.com/libunwind/libunwind.git
+        REPO_BRANCH v1.8.3)
 
     include(ConfigLibunwind)
 else()
